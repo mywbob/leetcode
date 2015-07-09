b7603ed231caa9b0a3234130c3b593deb3cbfd71
mywbob <mywbob@gmail.com>
2015-07-09 14:28:20 -0400

    tricky

---
+++ Factorial Trailing Zeroes.java
@@ -0,0 +1,17 @@
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+*/
+
+public class Solution {
+    //o(logn), pairs of factors of 2 and 5 in the prime factorization of n! generate trailing zeros, find how many 5s
+    public int trailingZeroes(int n) {
+        int rs = 0;
+        while (n != 0) {
+            rs += (n / 5);
+            n /= 5;
+        }
+        return rs;
+    }
+}

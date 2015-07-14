15e39b0d6cf6df5a5a8c2d6940602bc428ad00ff
mywbob <mywbob@gmail.com>
2015-07-14 19:13:26 -0400

    care the stop condition

---
+++ Linked List Cycle.java
@@ -0,0 +1,38 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+ 
+ //two ptrs
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode runner = head;
+        ListNode walker = head;
+        if (head == null || head.next == null) return false;
+        
+        while (!(runner == null || runner.next == null)) {
+            runner = runner.next.next;
+            walker = walker.next;
+            if (runner == walker) return true;
+        }
+        
+        return false;
+        
+    }
+}

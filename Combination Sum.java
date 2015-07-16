651a8886abd317c4db5df928e1601eee7170fd19
mywbob <mywbob@gmail.com>
2015-07-16 14:42:25 -0400

    7/16/2015 added, important

---
+++ Combination Sum.java
@@ -0,0 +1,45 @@
+/*
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+*/
+
+
+public class Solution {
+    public List<List<Integer>> res = new ArrayList<List<Integer>>();
+    List<Integer> ares = new ArrayList<Integer>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) return res;
+        Arrays.sort(candidates);
+        dfs(candidates, 0, 0, target);
+        return res;
+    }
+    
+    public void dfs(int[] candidates, int i, int sum, int target) {
+        if (sum == target) {
+            //add a res
+            res.add(new ArrayList<Integer>(ares));
+            //ares.remove(ares.size() - 1);
+            return;
+        }
+        
+        if (sum > target) {//
+            return;
+        }
+
+        for (int index = i; index < candidates.length ; index++) {
+            ares.add(candidates[index]);
+            dfs(candidates, index, sum + candidates[index], target);
+            ares.remove(ares.size() - 1);
+        }
+    }
+}

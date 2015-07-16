85abe15a68f04238554bebfcd212833cfe450bbb
mywbob <mywbob@gmail.com>
2015-07-15 23:07:03 -0400

    7/15/2015 added, IMPORTANT, REVIEW THE IDEA

---
+++ Binary Tree Postorder Traversal.java
@@ -0,0 +1,40 @@
+/*
+Given a binary tree, return the postorder traversal of its nodes' values.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> parentStack = new Stack<TreeNode>();
+        TreeNode lastNodeVisited = null;
+        
+        while (!parentStack.empty() || root != null) {
+            if (root != null) {
+                parentStack.push(root);
+                root = root.left;
+            }
+            else {
+                TreeNode peekNode = parentStack.peek();
+                if (peekNode.right != null && lastNodeVisited != peekNode.right)
+                /* if right child exists AND traversing node from left child, move right */
+                    root = peekNode.right;//change root
+                else {
+                    parentStack.pop();
+                    res.add(peekNode.val);//visit node;
+                    lastNodeVisited = peekNode;
+                }
+            }
+        }
+        return res;
+    }
+}

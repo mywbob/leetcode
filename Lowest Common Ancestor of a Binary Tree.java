696ec6b6a8e44a8d511023097653469ca93cd93b
mywbob <mywbob@gmail.com>
2015-07-17 14:27:40 -0400

    7/16/2015 added, important idea

--- Lowest Common Ancestor of a Binary Tree.java
+++ Lowest Common Ancestor of a Binary Tree.java
@@ -25,6 +25,7 @@ For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another exa
  */
  
  //http://articles.leetcode.com/2011/07/lowest-common-ancestor-of-a-binary-tree-part-i.html
+ //http://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
  /*
  1:The idea is to traverse the tree starting from root. If any of the given keys (n1 and n2) matches with root, then root is LCA (assuming that both keys are present). If root doesn’t match with any of the keys, we recur for left and right subtree. The node which has one key present in its left subtree and the other key present in right subtree is the LCA. If both keys lie in left subtree, then left subtree has LCA also, otherwise LCA lies in right subtree. q, p need exist in this tree.
  2: cnt p, q = 0,1,2, then like bst case 

35fd9f39a0afab65e79b23e04f6deca0d971a735
mywbob <mywbob@gmail.com>
2015-08-21 18:19:53 -0400

    8/21/2015 added, lazy

---
+++ Pow(x, n).java
@@ -0,0 +1,50 @@
+/*
+Implement pow(x, n).
+*/
+
+
+
+/*
+public class Solution {
+    public double myPow(double x, int n) {
+        if (x == 0) return 1;
+        if (x == 1) return 1;
+        if (n == 0) return 1;
+        double half = myPow(x, n/2);
+        
+        if (x >= 0 && n >= 0) {
+            if (n % 2 == 0) return half*half;
+            else return half*half*x;
+        } else if (x>0 && n< 0) {
+            if (n % 2 == 0) return half*half;
+            else return half*half*(1/x);
+        } else if (x<0 && n > 0) {
+            if (n % 2 == 0) return half*half;
+            else return half*half*x;
+        } else {
+            if (n % 2 == 0) return half*half;
+            else return half*half*(1/x);
+        }
+    }
+}
+*/
+
+
+
+public class Solution {
+    public double myPow(double x, int n) {
+        if (x == 0) return 1;
+        if (x == 1) return 1;
+        if (n == 0) return 1;
+        double half = myPow(x, n/2);
+        
+        if ( n >= 0) {
+            if (n % 2 == 0) return half*half;
+            else return half*half*x;
+
+        } else {
+            if (n % 2 == 0) return half*half;
+            else return half*half*(1/x);
+        }
+    }
+}

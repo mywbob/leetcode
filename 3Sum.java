2b60dd7796c0742daa5a0158f2aa0addc540f0e4
mywbob <mywbob@gmail.com>
2015-08-26 14:42:28 -0400

    1

--- 3Sum.java
+++ 3Sum.java
@@ -13,6 +13,55 @@ The solution set must not contain duplicate triplets.
 
 
 public class Solution {
+	//find all, use set. how to use set for list
+    //find and skip dup
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> temp = new ArrayList<Integer>();
+        for (int i=0; i<nums.length-2; ) {
+            int start = i+1;
+            int end=nums.length-1;
+            while (start < end) {
+                temp.clear();
+                if (nums[i] + nums[start] + nums[end] == 0) {
+                    temp.add(nums[i]);
+                    temp.add(nums[start]);
+                    temp.add(nums[end]);
+                    res.add(new ArrayList<Integer>(temp));
+                    
+                    //move start and end, skip dup
+                    int ss = start+1;
+                    while (ss < nums.length && nums[ss] == nums[start]) {
+                        ss++;
+                    }
+                    start = ss;
+                    
+                    int ee = end-1;
+                    while (ee >= 0 && nums[ee] == nums[end]) {
+                        ee--;
+                    }
+                    end = ee;
+
+                } else if (nums[i] + nums[start] + nums[end] < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+            //skip i if dup
+            int ii = i + 1;
+            while (ii < nums.length && nums[ii] == nums[i]) {
+                ii++;
+            }
+            i = ii;
+        }
+        
+        return res;
+    }
+	
+	
+	
     /*
     main idea, two pointers, move the pointers to reduce the dup
     sort the array first so the result in non descending order. 

686c1edb37871bf4c488fd4f226626f1929b738f
mywbob <mywbob@gmail.com>
2015-08-26 14:52:53 -0400

    1

--- 3Sum.java
+++ 3Sum.java
@@ -59,48 +59,4 @@ public class Solution {
         
         return res;
     }
-	
-	
-	
-    /*
-    main idea, two pointers, move the pointers to reduce the dup
-    sort the array first so the result in non descending order. 
-    */
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        if (nums == null || nums.length < 3) return res;
-        Arrays.sort(nums);
-        for (int i=0; i<nums.length-2; ) {
-            int start = i+1;
-            int end = nums.length-1;
-            while (start < end) {
-                if (nums[i] + nums[start] + nums[end] == 0) {
-                    //add res
-                    List<Integer> alist = new ArrayList<Integer>();
-                    alist.add(nums[i]);
-                    alist.add(nums[start]);
-                    alist.add(nums[end]);
-                    res.add(alist);
-                    while (start+1 < nums.length && nums[start+1] == nums[start]) {
-                        start++;
-                    }
-                    
-                    while (end-1 > i && nums[end-1] == nums[end]) {
-                        end--;
-                    }
-                    start++;
-                    end--;
-                } else if (nums[i] + nums[start] + nums[end] < 0) {
-                    start++;
-                } else {
-                    end--;
-                }
-            }
-            //skip i
-            while (i + 1 < nums.length-2  && nums[i+1] == nums[i]) i++; 
-            i++;
-            
-        }
-        return res;
-    }
 }

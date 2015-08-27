3a0461bc50d9d51422276507cd9850526089e1fc
mywbob <mywbob@gmail.com>
2015-08-26 20:06:39 -0400

    3

--- Factorial Trailing Zeroes.java
+++ Factorial Trailing Zeroes.java
@@ -5,6 +5,17 @@ Note: Your solution should be in logarithmic time complexity.
 */
 
 public class Solution {
+	public int trailingZeroes(int n) {
+        int res = 0;
+        while (n != 0) {
+            int numofnumwith5 = n/5;
+            res = res + numofnumwith5;
+            n = n / 5;
+        }
+        return res;
+    }
+	
+	//old
     //o(logn), pairs of factors of 2 and 5 in the prime factorization of n! generate trailing zeros, find how many 5s
     public int trailingZeroes(int n) {
         int rs = 0;

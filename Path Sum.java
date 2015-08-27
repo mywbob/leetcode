4a2fb2c408bb141175b82ec0679263b876a6d67c
mywbob <mywbob@gmail.com>
2015-08-27 19:20:34 -0400

    3

--- Path Sum.java
+++ Path Sum.java
@@ -24,6 +24,15 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
  * }
  */
 public class Solution {
+	//care, must reach leaf
+	public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && sum == root.val) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+	
+	//old
+	
     /*
     //wrong, 1,2 1 case, have to reach the leaf!!
     public boolean hasPathSum(TreeNode root, int sum) {

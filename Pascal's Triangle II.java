5df32346d677613e57d2d665a68290607a6fa64d
mywbob <mywbob@gmail.com>
2015-08-27 19:03:50 -0400

    3

--- Pascal's Triangle II.java
+++ Pascal's Triangle II.java
@@ -10,6 +10,23 @@ Could you optimize your algorithm to use only O(k) extra space?
 
 
 public class Solution {
+	//no need the array
+	public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<Integer>();
+        int[] temp = new int[rowIndex+1];
+        temp[0] = 1;
+        for (int i = 0; i<rowIndex; i++) {
+            for (int j =i+1; j>0; j--) {
+                temp[j] = temp[j] + temp[j-1];
+            }
+        }
+        
+        for (int i=0; i<temp.length; i++) {
+            res.add(temp[i]);
+        }
+        return res;
+    }
+	
     //1: keep a prev and curr list o(2k) space
     //2: backward, one list
     public List<Integer> getRow(int rowIndex) {//very clean 

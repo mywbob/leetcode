6db3158e4a77287f5638d217fc5336a6d32058b2
mywbob <mywbob@gmail.com>
2015-08-28 18:46:58 -0400

    1

--- Subsets.java
+++ Subsets.java
@@ -24,6 +24,35 @@ combination idea
 
 
 public class Solution {
+	List<List<Integer>> res = new ArrayList<List<Integer>>();
+    List<Integer> temp = new ArrayList<Integer>();
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=1; i<=nums.length; i++) {
+            combine(i, 0, nums);
+        }
+        res.add(new ArrayList<Integer>());
+        return res;
+    }
+    
+    public void combine(int k, int j, int[] nums) {
+        if (temp.size() == k) {
+            res.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        
+        for (int i= j; i<nums.length; i++) {
+            temp.add(nums[i]);
+            combine(k, i+1, nums);
+            temp.remove(temp.size() - 1);
+        }
+    }
+	
+	
+	
+	
+	
+	//old
     List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> subsets(int[] nums) {
         if (nums == null || nums.length == 0) return res;

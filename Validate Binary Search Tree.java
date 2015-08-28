e80b53e45627404720a72b2095a7c22b985f7cf2
mywbob <mywbob@gmail.com>
2015-08-28 11:44:26 -0400

    1

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -18,6 +18,22 @@ Both the left and right subtrees must also be binary search trees.
  *     TreeNode(int x) { val = x; }
  * }
  */
+ public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean check(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+        return check(root.left, min, root.val) && check(root.right, root.val, max);
+        
+    }
+}
+ 
+ 
+ 
+ 
  //1 inorder store, check if sorted, dfs idea
  //2 max min, overflow !
  /*

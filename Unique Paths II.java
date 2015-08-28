5554c6329e95c63cf6ab1b6b70f0718db21c13af
mywbob <mywbob@gmail.com>
2015-08-28 14:40:17 -0400

    1

--- Unique Paths II.java
+++ Unique Paths II.java
@@ -20,6 +20,30 @@ Note: m and n will be at most 100.
 
 
 public class Solution {
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] table = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i=0; i<obstacleGrid.length; i++) {
+            if (obstacleGrid[i][0] == 0) table[i][0] = 1;
+            else break;
+        }
+        
+        for (int i=0; i<obstacleGrid[0].length; i++) {
+            if (obstacleGrid[0][i] == 0) table[0][i] = 1;
+            else break;
+        }
+        
+        for (int i=1; i<obstacleGrid.length; i++) {
+            for (int j=1; j<obstacleGrid[0].length; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    table[i][j] = table[i-1][j] + table[i][j-1];
+                }
+            }
+        }
+        
+        return table[table.length-1][table[0].length-1];
+    }
+	
+	//old
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int[][] table = new int[obstacleGrid.length][obstacleGrid[0].length];
         for (int i=0; i< table.length; i++) {

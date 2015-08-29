6f27fbb4643e040a83a7d3fb43743401451f2859
mywbob <mywbob@gmail.com>
2015-08-29 16:06:32 -0400

    2

--- Divide Two Integers.java
+++ Divide Two Integers.java
@@ -1,4 +1,37 @@
 public class Solution {
+	public int divide(int dividend, int divisor) {
+        if (dividend == 0) return 0;
+        boolean isPos = false;
+        if (dividend > 0 && divisor > 0 || dividend < 0 && divisor < 0) isPos = true;
+        long dd = Math.abs((long)dividend);
+        long dr = Math.abs((long)divisor);
+        long res = 0;
+        while (dd >= dr) {
+            long temp = 1;
+            long tempdr = dr;
+            while (dd > (tempdr << 1)) {
+                temp = temp << 1;
+                tempdr = tempdr << 1;
+            }
+            dd = dd - tempdr;
+            res = res + temp;
+            
+        }
+        
+        if (isPos) {
+            if (res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            else return (int)res;
+        } else {
+            if (-res < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            else return (int)(-res);
+        }
+    }
+	
+	
+	
+	
+	
+	//old
     public int divide(int dividend, int divisor) {
         long dd = Math.abs((long)(dividend));
         long dr = Math.abs((long)(divisor));

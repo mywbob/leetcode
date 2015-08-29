e97bfd7666e880f273cbe753d7d994f3b975a009
mywbob <mywbob@gmail.com>
2015-08-29 18:28:22 -0400

    2

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -5,6 +5,28 @@ Compute and return the square root of x.
 */
 
 public class Solution {
+	
+	//overflow
+    //bs: number and array, treat number as array, 1,2,3,4,->[0,1,2,3,4] index == value 
+    public int mySqrt(int x) {
+        return bs(0, x, x);
+    }
+    
+    public int bs(int s, int e, int target) {
+        if (s > e) return -1;// not possible
+        int mid = s + (e-s)/2;
+        if (mid == 0) return target;//mid == 0?
+        if (mid <= target / mid && (mid + 1) > target / (mid+1)) {
+            return mid;
+        } else if (mid > target / mid) {
+            return bs(s, mid-1, target);
+        } else {
+            return bs(mid+1, e, target);
+        }
+    }
+	
+	
+	//old
     //overflow! use divide
     public int mySqrt(int x) {
         if (x == 0) return 0;

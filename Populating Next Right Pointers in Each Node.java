56fcb5e81cebd00327fe9e808880bd3742b3d226
mywbob <mywbob@gmail.com>
2015-08-29 20:05:37 -0400

    1

--- Populating Next Right Pointers in Each Node.java
+++ Populating Next Right Pointers in Each Node.java
@@ -44,15 +44,18 @@ After calling your function, the tree should look like:
 node.right.next = node.next.left
  */
 public class Solution {
-    //wrong! missing the mid link
-    public void connect(TreeLinkNode root) {
+	public void connect(TreeLinkNode root) {
         if (root == null) return;
+
         if (root.left != null) {
             root.left.next = root.right;
             if (root.next != null)
                 root.right.next = root.next.left;
+            else
+                root.right.next = null;
         }
-
+        
+        
         connect(root.left);
         connect(root.right);
     }

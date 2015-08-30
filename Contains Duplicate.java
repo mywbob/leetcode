740dd98ec628a4c63192cf87ea574d0ebc8f5b04
mywbob <mywbob@gmail.com>
2015-08-30 13:29:10 -0400

    1

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -4,6 +4,25 @@ Given an array of integers, find if the array contains any duplicates. Your func
 
 
 public class Solution {
+	HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+    public boolean containsDuplicate(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i])+1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        
+        for (Integer i : map.keySet()) {
+            if (map.get(i)>1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+	
     //set o(1) time, o(n) space
     //for loops, o(n^2) time
     public boolean containsDuplicate(int[] nums) {

a546073d183d4d076b9c257804cd23e52ab485b1
mywbob <mywbob@gmail.com>
2015-08-30 12:13:58 -0400

    1

--- Best Time to Buy and Sell Stock.java
+++ Best Time to Buy and Sell Stock.java
@@ -14,18 +14,20 @@ public class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
         int[] changes = new int[prices.length-1];
-        for (int i = 1; i<prices.length;i++) {
-            changes[i-1] = prices[i] - prices[i-1];
+        for (int i=0; i<changes.length;i++) {
+            changes[i] = prices[i+1] - prices[i]; 
         }
-        int sum = 0;
-        int max = 0;
+        
+        int max=0;
+        int sum=0;
         for (int i=0; i<changes.length; i++) {
             sum = sum + changes[i];
             max = Math.max(max, sum);
             if (sum < 0) {
-                sum = 0;//reset
+                sum = 0;
             }
         }
+        
         return max;
     }
 }

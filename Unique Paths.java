1aa2a9c9a884da720f82b64572e63612ccfc5d1e
mywbob <mywbob@gmail.com>
2015-08-29 21:39:24 -0400

    1

--- Unique Paths.java
+++ Unique Paths.java
@@ -14,20 +14,17 @@ Note: m and n will be at most 100.
 public class Solution {
     public int uniquePaths(int m, int n) {
         int[][] table = new int[m][n];
-        for (int i=0; i< m; i++) {
+        for (int i=0; i<m; i++) {
             table[i][0] = 1;
         }
-        
-        for (int j=0; j<n; j++) {
-            table[0][j] = 1;
+        for (int i=0; i<n; i++) {
+            table[0][i] = 1;
         }
-        
-        for (int i=1; i<m;i++) {
-            for (int j=1; j<n;j++) {
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
                 table[i][j] = table[i-1][j] + table[i][j-1];
             }
         }
-        
         return table[m-1][n-1];
     }
 }

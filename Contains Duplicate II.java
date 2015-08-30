c32a0edca764a7c5967745f4e47c872ce23509dc
mywbob <mywbob@gmail.com>
2015-08-30 13:35:28 -0400

    1

--- Contains Duplicate II.java
+++ Contains Duplicate II.java
@@ -4,6 +4,22 @@ Given an array of integers and an integer k, find out whether there there are tw
 
 
 public class Solution {
+	HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) return true;
+                else map.put(nums[i], i);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+	
+	
+	
+	//old
     //o(n)
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();

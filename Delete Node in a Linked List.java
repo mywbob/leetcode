b9e21163102f01c8b80801ab8e9703dc94f3276c
mywbob <mywbob@gmail.com>
2015-08-30 16:35:36 -0400

    8/30/2015 added, tricky and easy to make mistakes

---
+++ Delete Node in a Linked List.java
@@ -0,0 +1,32 @@
+/*
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+*/
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode perv = null;
+        ListNode cur = node;
+        ListNode next = cur.next;
+        while (next!= null) {
+            cur.val = next.val;
+            perv = cur;
+            cur = next;
+            next = cur.next;
+        }
+        //cur = null;//does not work!
+        perv.next = null;
+    }
+}

65d08c55ab373e305ea206b8e24ff64b6694a30b
mywbob <mywbob@gmail.com>
2015-08-31 18:43:55 -0400

    1

--- Group Anagrams.java
+++ Group Anagrams.java
@@ -17,30 +17,30 @@ All inputs will be in lower-case.
 
 
 public class Solution {
+    //sort string arr, or sort the list
     public List<List<String>> groupAnagrams(String[] strs) {
-        Arrays.sort(strs);
+        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
         List<List<String>> res = new ArrayList<List<String>>();
-        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        if (strs == null || strs.length == 0) return res;
+        Arrays.sort(strs);//important
         for (int i=0; i<strs.length; i++) {
-            char[] temp = strs[i].toCharArray();
-            Arrays.sort(temp);
-            String key = new String(temp);
-            
-            if (map.containsKey(key))  {
-                ArrayList<String> t = map.get(key);
-                t.add(strs[i]);
-                map.put(key, t);
+            char[] chararr = strs[i].toCharArray();
+            Arrays.sort(chararr);
+            String ss = new String(chararr);
+            if (map.containsKey(ss)) {
+                List<String> temp = map.get(ss);
+                temp.add(strs[i]);
+                map.put(ss, temp);
             } else {
-                ArrayList<String> n = new ArrayList<String>();
-                n.add(strs[i]);
-                map.put(key, n);
+                List<String> temp = new ArrayList<String>();
+                temp.add(strs[i]);
+                map.put(ss, temp);
             }
         }
         
         for (String key : map.keySet()) {
             res.add(map.get(key));
         }
-        
         
         return res;
     }

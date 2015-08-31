8772a39805f7cdb00212012e29c843a324a179a0
mywbob <mywbob@gmail.com>
2015-08-31 18:10:27 -0400

    1

--- Binary Tree Zigzag Level Order Traversal.java
+++ Binary Tree Zigzag Level Order Traversal.java
@@ -29,20 +29,22 @@ return its zigzag level order traversal as:
  */
 public class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> ares = new ArrayList<Integer>();
         Queue<TreeNode> cur = new LinkedList<TreeNode>();
         Queue<TreeNode> next = new LinkedList<TreeNode>();
-        Queue<TreeNode> temp;
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> row = new ArrayList<Integer>();
+        Queue<TreeNode> temp = null;
+        boolean reverse = false;
         if (root == null) return res;
         cur.add(root);
         while (!cur.isEmpty()) {
             TreeNode node = cur.poll();
-            row.add(node.val);
-            if (node.left != null) {
+            ares.add(node.val);
+            if (node.left!= null) {
                 next.add(node.left);
             }
-            if (node.right != null) {
+            
+            if (node.right!=null) {
                 next.add(node.right);
             }
             
@@ -50,18 +52,15 @@ public class Solution {
                 temp = cur;
                 cur = next;
                 next = temp;
-                res.add(new ArrayList<Integer>(row));
-                row.clear();
+                if (reverse) Collections.reverse(ares);
+                reverse = !reverse;
+                res.add(new ArrayList<Integer>(ares));
+                ares.clear();
             }
         }
-        boolean reverse = false;
-        for (List<Integer> l : res) {
-            if (reverse) {
-                Collections.reverse(l);
-            } 
-            
-            reverse = !reverse;
-        }
+        
+        
+        
         
         return res;
     }

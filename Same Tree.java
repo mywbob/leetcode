b7ab8594240c63044d5a957d3b6f0f11e3c2fe24
mywbob <mywbob@gmail.com>
2015-08-31 18:21:58 -0400

    1

--- Same Tree.java
+++ Same Tree.java
@@ -17,13 +17,9 @@ Two binary trees are considered equal if they are structurally identical and the
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        else if (p != null && q == null) return false;
-        else if (p == null && q != null) return false;
-        else { //(p != null && q != null)
-            if (p.val != q.val) return false;
-        }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p == null && q== null) return true;
+        if (p == null && q!= null || p != null && q== null|| p.val != q.val) return false;
         
+        return isSameTree(p.left, q.left)&& isSameTree(p.right, q.right);
     }
 }

06ffe4d38b25c7e3d35786ba1a1a5e0397e62b62
mywbob <mywbob@gmail.com>
2015-09-02 19:58:19 -0400

    1

--- Maximum Depth of Binary Tree.java
+++ Maximum Depth of Binary Tree.java
@@ -20,7 +20,6 @@ public class Solution {
         if (root == null) return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
-        
+        return Math.max(left, right)+1;
     }
 }

d7b5521486736065d0469d7b1fc5c77531ff98f6
mywbob <mywbob@gmail.com>
2015-09-02 19:37:34 -0400

    1

--- Invert Binary Tree.java
+++ Invert Binary Tree.java
@@ -24,6 +24,19 @@ to
 9   6 3   1
 */
 public class Solution {
+	public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        
+        return root;
+    }
+	
+	
+	
+	//old
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
         swap(root);

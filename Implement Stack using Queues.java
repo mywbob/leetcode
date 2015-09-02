f2dd0dc89a48b6bc21fcc475d8808347f19551b7
mywbob <mywbob@gmail.com>
2015-09-02 19:52:59 -0400

    1

--- Implement Stack using Queues.java
+++ Implement Stack using Queues.java
@@ -1,5 +1,5 @@
 class MyStack {
-    //keep one q empty
+    
     Queue<Integer> q1 = new LinkedList<Integer>();
     Queue<Integer> q2 = new LinkedList<Integer>();
     
@@ -11,43 +11,36 @@ class MyStack {
     // Removes the element on top of the stack.
     public void pop() {
         if (q1.isEmpty()) {
-            put_n_1_backto1();
-            q2.poll();
-        } else {
-            put_n_1_to2();
-            q1.poll();
+            while (!q2.isEmpty()) {
+                q1.add(q2.poll());
+            }
+        } 
+           
+        while (q1.size() != 1) {
+            q2.add(q1.poll());
         }
+        
+        q1.poll();
+        
     }
 
     // Get the top element.
     public int top() {
-        int top;
         if (q1.isEmpty()) {
-            put_n_1_backto1();
-            top= q2.peek();
-            q1.add(q2.poll());
-        } else {
-            put_n_1_to2();
-            top= q1.peek();
+            while (!q2.isEmpty()) {
+                q1.add(q2.poll());
+            }
+        } 
+           
+        while (q1.size() != 1) {
             q2.add(q1.poll());
         }
-        return top;
+        
+        return q1.peek();
     }
 
     // Return whether the stack is empty.
     public boolean empty() {
         return q1.isEmpty() && q2.isEmpty();
-    }
-    
-    public void put_n_1_backto1() {
-        while (q2.size() > 1) {
-            q1.add(q2.poll());
-        }
-    }
-    
-    public void put_n_1_to2(){
-        while (q1.size() >1) {
-            q2.add(q1.poll());
-        }
     }
 }

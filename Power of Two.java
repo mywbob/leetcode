e7dd8a4a58a8ae4767fc5958ff60287c060e2a02
mywbob <mywbob@gmail.com>
2015-09-02 20:09:36 -0400

    2

--- Power of Two.java
+++ Power of Two.java
@@ -4,6 +4,17 @@ Given an integer, write a function to determine if it is a power of two.
 
 
 public class Solution {
+	
+	//1:use mask, 1, 10, 100, check mask ^ n == 0  in a loop
+    //2:clear rightmore bit
+    //overflow, or use n > 0
+    public boolean isPowerOfTwo(int n) {
+        if (n==0) return false;
+        long t = n;
+        return (t & (t-1)) == 0;
+    }
+	
+	//old
     //1: bit operation
     //2: power of 2 numbers: 10, 100, 1000
     //3: turn off the right most 1 bit of n -> n & (n-1)

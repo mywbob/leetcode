e18bad9fb80d2462ff2f2863cae0cfc575008caf
mywbob <mywbob@gmail.com>
2015-09-03 19:50:08 -0400

    1

--- Implement Queue using Stacks.java
+++ Implement Queue using Stacks.java
@@ -8,43 +8,39 @@ empty() -- Return whether the queue is empty.
 */
 
 class MyQueue {
-    Stack<Integer> st1 = new Stack<Integer>();
-    Stack<Integer> st2 = new Stack<Integer>();
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
     // Push element x to the back of queue.
     public void push(int x) {
-        st1.push(x);
+        stack1.push(x);
     }
 
     // Removes the element from in front of queue.
     public void pop() {
-        if (st2.empty()) {
-            pour();
-            st2.pop();
-            
+        if (!stack2.isEmpty()) {
+            stack2.pop();
         } else {
-            st2.pop();
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            stack2.pop();
         }
     }
 
     // Get the front element.
     public int peek() {
-        if (st2.empty()) {
-            pour();
-            return st2.peek();
+        if (!stack2.isEmpty()) {
+           return stack2.peek();
         } else {
-            return st2.peek();  
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.peek();
         }
     }
 
     // Return whether the queue is empty.
     public boolean empty() {
-        return st1.empty() && st2.empty();
+        return stack1.isEmpty() && stack2.isEmpty();
     }
-    
-    public void pour() {
-        while (!st1.empty()) {
-            st2.push(st1.pop());
-        }
-    }
-	
 }

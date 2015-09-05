5568ec9b558d39e48febc7a3a6ff0377ea726522
mywbob <mywbob@gmail.com>
2015-09-05 18:37:54 -0400

    1

--- Roman to Integer.java
+++ Roman to Integer.java
@@ -19,23 +19,22 @@ M	1,000
 
 public class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        int res = map.get(s.charAt(s.length()-1));
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
         
-        for (int i = s.length() - 1; i > 0; i--) {
+        int res = map.get(s.charAt(s.length()-1));
+        for (int i=s.length()-1; i > 0; i--) {
             if (map.get(s.charAt(i)) > map.get(s.charAt(i-1))) {
                 res = res - map.get(s.charAt(i-1));
             } else {
                 res = res + map.get(s.charAt(i-1));
             }
-            
         }
         
         return res;

4f231f5e84e0d0071c517cb0ad5f28fa08df983b
mywbob <mywbob@gmail.com>
2015-09-05 18:12:01 -0400

    1

--- Implement strStr().java
+++ Implement strStr().java
@@ -9,17 +9,18 @@ public class Solution {
     //1:bf
     //2:kmp, but i dont know
     public int strStr(String haystack, String needle) {
-        if (haystack == null) return -1;
-        if (needle == null || needle.length() == 0) return 0;
-        if (haystack.length() < needle.length()) return -1;
-        
-        int index = 0;
+        if (haystack == null || needle == null || haystack.length() < needle.length()) return -1;
+        if (needle.length() == 0) return 0;
         for (int i=0; i<= haystack.length() - needle.length(); i++) {
-            for (int j = 0; j< needle.length(); j++) {
-                if (haystack.charAt(i+j) != needle.charAt(j)) break;
-                if (j == needle.length() - 1) return i;
+            if (needle.charAt(0) == haystack.charAt(i)) {
+                int j = 0;
+                for (j=0; j<needle.length();j++) {
+                    if (needle.charAt(j) != haystack.charAt(i+j)) break;
+                }
+                if (j == needle.length()) return i;
             }
         }
+        
         return -1;
     }
 }

4073b5bc949c267402de3a1df65990fe0e9d4704
mywbob <mywbob@gmail.com>
2015-09-06 15:02:31 -0400

    1

--- Two Sum.java
+++ Two Sum.java
@@ -12,20 +12,19 @@ Output: index1=1, index2=2
 
 
 public class Solution {
+    Map<Integer, Integer> map =new HashMap<Integer, Integer>();
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         int[] res = new int[2];
-        for (int i=0; i< nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                res[0] = map.get(nums[i]) + 1;
+        for (int i=0; i<nums.length;i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(target-nums[i], i);
+            } else {
+                res[0] = map.get(nums[i])+1;
                 res[1] = i+1;
                 break;
-            } else {
-                map.put(target - nums[i], i);
             }
-            
         }
-        return res;
         
+        return res;
     }
 }

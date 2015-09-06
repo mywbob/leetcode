a3975372d26c26de1005c4ecc6fba497246dc81e
mywbob <mywbob@gmail.com>
2015-09-06 14:48:26 -0400

    1

--- Word Search.java
+++ Word Search.java
@@ -19,6 +19,66 @@ word = "ABCB", -> returns false.
 ////return as soon as a match is found
 
 public class Solution {
+	boolean res = false;
+    public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int i=0; i<board.length; i++) {
+            for (int j=0; j<board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {//first char match
+                    visited[i][j] = true;
+                    dfs(i, j, word.substring(1, word.length()), board, visited);
+                    visited[i][j] = false;
+                    if (res == true) return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public void dfs(int r, int c, String str, char[][] board, boolean[][] visited) {
+        if (str.length() == 0) {
+            res = true;
+            return;
+        }
+        if (res == true) return;
+        //top
+        if (valid(r-1, c, str, board, visited)) {
+            visited[r-1][c] = true;
+            dfs(r-1,c,str.substring(1, str.length()), board, visited);
+            visited[r-1][c] = false;
+        }
+        //right
+        if (valid(r, c+1, str, board, visited)) {
+            visited[r][c+1] = true;
+            dfs(r,c+1,str.substring(1, str.length()), board, visited);
+            visited[r][c+1] = false;
+        }
+        //bot
+        if (valid(r+1, c, str, board, visited)) {
+            visited[r+1][c] = true;
+            dfs(r+1,c,str.substring(1, str.length()), board, visited);
+            visited[r+1][c] = false;
+        }
+        //left
+        if (valid(r, c-1, str, board, visited)) {
+            visited[r][c-1] = true;
+            dfs(r,c-1,str.substring(1, str.length()), board, visited);
+            visited[r][c-1] = false;
+        }
+        
+    }
+    
+    public boolean valid(int r, int c, String s, char[][] board, boolean[][] visited) {
+        if (r>=0 && r<board.length && c>=0 && c<board[0].length && visited[r][c] == false && s.charAt(0)== board[r][c]) return true;
+        return false;
+    }
+	
+	
+	
+	
+	
+	
+	//old
     boolean[] res = new boolean[1];
     
     public boolean exist(char[][] board, String word) {

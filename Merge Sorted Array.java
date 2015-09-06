571c2595c7f972363e79e024ea307a9857df160f
mywbob <mywbob@gmail.com>
2015-09-05 21:31:54 -0400

    1

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -10,18 +10,17 @@ public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int index1 = m-1;
         int index2 = n-1;
-        for (int i= m+n-1; i>=0; i--) {
-            if (index1 < 0 || index2 < 0) break;
-            
-            if (nums1[index1] >= nums2[index2]) {
-                nums1[i] = nums1[index1--];
+        int cur = m+n-1;
+        while (index1>=0 && index2>=0) {
+            if (nums1[index1] < nums2[index2]) {
+                nums1[cur--] = nums2[index2--];
             } else {
-                nums1[i] = nums2[index2--];
+                nums1[cur--] = nums1[index1--];
             }
         }
         
-        if (index1 < 0) {
-            while (index2 >= 0) {nums1[index2] = nums2[index2]; index2--;}
+        while (index2>=0) {
+            nums1[cur--] = nums2[index2--];  
         }
         
     }

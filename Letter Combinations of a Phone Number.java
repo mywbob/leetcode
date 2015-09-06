523f57de79741f18e8ac6240888a7ea7b9952e43
mywbob <mywbob@gmail.com>
2015-09-05 20:05:17 -0400

    1

--- Letter Combinations of a Phone Number.java
+++ Letter Combinations of a Phone Number.java
@@ -4,6 +4,27 @@ Given a digit string, return all possible letter combinations that the number co
 
 
 public class Solution {
+	List<String> res = new ArrayList<String>();
+    String[] table = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) return res;
+        findAll(0, "", digits.length(), digits);
+        return res;
+    }
+    
+    public void findAll(int ith, String temp, int len, String digits) {
+        if (ith == len) {
+            res.add(new String(temp));
+            return;
+        }
+        char ithnum = digits.charAt(ith);
+        for (int i=0; i<table[(int)(ithnum - '0')].length(); i++) {
+            findAll(ith+1, temp + table[(int)(ithnum - '0')].charAt(i), len, digits);
+        }
+        
+    }
+	
+	//old
     List<String> res = new ArrayList<String>();
     public List<String> letterCombinations(String digits) {
         if (digits == null || digits.length() == 0) return res;

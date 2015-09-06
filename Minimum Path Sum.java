0e7b102453b76e83b85244c21c09b6b44c012010
mywbob <mywbob@gmail.com>
2015-09-05 21:39:25 -0400

    1

--- Minimum Path Sum.java
+++ Minimum Path Sum.java
@@ -6,22 +6,23 @@ Note: You can only move either down or right at any point in time.
 
 public class Solution {
     public int minPathSum(int[][] grid) {
-        int[][] table = new int[grid.length][grid[0].length];
-        table[0][0] = grid[0][0];
-        for (int i=1; i<grid.length; i++) {
-            table[i][0] = grid[i][0] + table[i-1][0];
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int i=0; i<grid.length; i++) {
+            if (i == 0) dp[i][0] = grid[i][0];
+            else dp[i][0] = grid[i][0] + dp[i-1][0]; 
         }
         
-        for (int i=1; i<grid[0].length;i++) {
-            table[0][i] = grid[0][i] + table[0][i-1]; 
+        for (int i=0; i<grid[0].length; i++) {
+            if (i == 0) dp[0][i] = grid[0][i];
+            else dp[0][i] = grid[0][i] + dp[0][i-1]; 
         }
         
-        for (int i=1; i< grid.length;i++) {
-            for (int j=1; j<grid[0].length;j++) {
-                table[i][j] = grid[i][j] + Math.min(table[i-1][j], table[i][j-1]);
+        for (int i =1; i<grid.length; i++) {
+            for (int j=1; j<grid[0].length; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
             }
         }
         
-        return table[grid.length-1][grid[0].length-1];
+        return dp[dp.length-1][dp[0].length-1];
     }
 }

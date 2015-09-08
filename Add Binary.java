adeec2f3bfa9656fdcd803bed0d1ed95f33d28b7
mywbob <mywbob@gmail.com>
2015-09-07 21:45:47 -0400

    1

--- Add Binary.java
+++ Add Binary.java
@@ -43,4 +43,42 @@ public class Solution {
         return res;
         
     }
+	
+	
+	
+	public String addBinary(String a, String b) {
+        if (a.length() == 0) return b;
+        if (b.length() == 0) return a;
+        if (a.length() < b.length()) {
+            String t = a;
+            a = b;
+            b = t;
+        }
+        String res ="";
+        int carry = 0;
+        int indexa = a.length() -1;
+        int indexb = b.length() -1;
+        while (indexb >=0) {
+            int bit = ((int)(b.charAt(indexb) - '0') + (int)(a.charAt(indexa) - '0') + carry) % 2;
+            carry = ((int)(b.charAt(indexb) - '0') + (int)(a.charAt(indexa) - '0') + carry) / 2;
+            res = bit + res;
+            indexa--;
+            indexb--;
+        }
+        
+        if (carry == 0) {
+            return a.substring(0, indexa+1) + res;
+        } else {
+            while (indexa >=0) {
+                int bit = ((int)(a.charAt(indexa) - '0') + carry) % 2;
+                carry = ((int)(a.charAt(indexa) - '0') + carry) / 2;
+                res = bit + res;
+                indexa--;
+            }
+            
+            if (carry == 0) return res;
+            else return "1" + res;
+        }
+        
+    }
 }

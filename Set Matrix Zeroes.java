9fc0ecc584218d901f2e8f875d5604dbfba311ee
mywbob <mywbob@gmail.com>
2015-09-07 20:17:56 -0400

    1

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -17,17 +17,15 @@ public class Solution {
     //2: o(m+n) space
     //3: o(1) space, use first row and col
     public void setZeroes(int[][] matrix) {
-        boolean firstrow = false;
-        boolean firstcol = false;
-        for (int i=0; i<matrix.length;i++) {
-            if (matrix[i][0] == 0) {firstcol = true; break;}
+        boolean firstRowHas0 = false;
+        boolean firstColHas0 = false;
+        for (int i=0; i<matrix.length; i++) {
+            if (matrix[i][0] == 0) firstRowHas0 = true;
         }
-        
-        for (int i=0; i<matrix[0].length;i++) {
-            if (matrix[0][i] == 0) {firstrow = true; break;}
+        for (int i=0; i<matrix[0].length; i++) {
+            if (matrix[0][i] == 0) firstColHas0 = true;
         }
-        
-        for (int i=1; i< matrix.length;i++) {
+        for (int i=1; i<matrix.length; i++) {
             for (int j=1; j<matrix[0].length;j++) {
                 if (matrix[i][j] == 0) {
                     matrix[0][j] = 0;
@@ -36,7 +34,7 @@ public class Solution {
             }
         }
         
-        for (int i=1; i< matrix.length;i++) {
+        for (int i=1; i<matrix.length; i++) {
             for (int j=1; j<matrix[0].length;j++) {
                 if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
@@ -44,16 +42,17 @@ public class Solution {
             }
         }
         
-        if (firstrow) {
+        if (firstRowHas0) {
+            for (int i=0; i<matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+        if (firstColHas0) {
             for (int i=0; i<matrix[0].length; i++) {
                 matrix[0][i] = 0;
             }
         }
         
-        if (firstcol) {
-            for (int i=0; i<matrix.length; i++) {
-                matrix[i][0] = 0;
-            }
-        }
     }
 }

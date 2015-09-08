59c5d638905452bdaf4ee8932db5e2975fff53f5
mywbob <mywbob@gmail.com>
2015-09-07 22:04:13 -0400

    1

--- Maximum Subarray.java
+++ Maximum Subarray.java
@@ -8,16 +8,14 @@ the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 
 public class Solution {
     public int maxSubArray(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        int sum=0;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
         for (int i=0; i<nums.length;i++) {
             sum = sum + nums[i];
             max = Math.max(max, sum);
-            
-            if (sum < 0) {
-                sum = 0;
-            } 
-            
+            if (sum<0) {
+                sum=0;
+            }
         }
         return max;
     }

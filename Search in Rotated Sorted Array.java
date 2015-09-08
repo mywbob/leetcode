e47abb4f62f02fac5e7e353b6271e3cefdd252a7
mywbob <mywbob@gmail.com>
2015-09-08 17:20:06 -0400

    1

--- Search in Rotated Sorted Array.java
+++ Search in Rotated Sorted Array.java
@@ -13,25 +13,25 @@ You may assume no duplicate exists in the array.
 
 public class Solution {
     public int search(int[] nums, int target) {
-        return bs(0, nums.length-1, target, nums);
+        return bs(0, nums.length-1, nums, target);
     }
     
-    public int bs(int s, int e, int target, int[] nums) {
-        int mid = (s + e)/2;
-        if (s > e) return -1;//do not forget
+    public int bs(int s, int e, int[] nums, int target) {
+        if (s>e) return -1;
+        int mid = (s+e)/2;
         if (nums[mid] == target) {
             return mid;
-        } else if (nums[mid] >= nums[s]) {//left in order
-            if (target < nums[mid] && target >= nums[s]) {
-                return bs(s, mid-1, target, nums);
-            } else {
-                return bs(mid+1, e, target, nums);
+        } else if (nums[mid] >= nums[s]) {//left sorted
+            if (nums[s] <= target && nums[mid] >= target) {//in left
+                return bs(s, mid-1, nums, target);
+            } else {//in right
+                return bs(mid+1, e, nums, target);
             }
-        } else {//right in order
-            if (target > nums[mid] && target <= nums[e]) {
-                return bs(mid+1, e, target, nums);
-            } else {
-                return bs(s, mid-1, target, nums);
+        } else {//right sorted
+            if (nums[mid] <= target && nums[e] >= target) {//in right
+                return bs(mid+1, e, nums, target);
+            } else {//in left
+                return bs(s, mid-1, nums, target);
             }
         }
     }

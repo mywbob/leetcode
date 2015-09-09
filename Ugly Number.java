4168ff8c7be92428a5ceec3639acd0e5287386f4
mywbob <mywbob@gmail.com>
2015-09-08 20:25:54 -0400

    1

--- Ugly Number.java
+++ Ugly Number.java
@@ -7,6 +7,19 @@ Note that 1 is typically treated as an ugly number.
 */
 
 public class Solution {
+	public boolean isUgly(int num) {
+        if (num == 0) return false;
+        while (num % 2 == 0) num = num / 2;
+        while (num % 3 == 0) num = num / 3;
+        while (num % 5 == 0) num = num / 5;
+        
+        return num == 1;
+        
+    }
+	
+	
+	
+	//old
     //1: bf, tle
     //2: keep divide 2,3,5 to get a new number. the new number must be an odd number. 2 cases: 1) new number is prime, return false. 2) new number is not a prime, then it must be the production of 2 odd numbers(the base factor 1,3,5,7,9 => 7 left). whatever still false... hard to prove..
     

d7539dc452dbce1e2687006cc91db0882a397e3a
mywbob <mywbob@gmail.com>
2015-09-10 21:16:26 -0400

    2

--- Count Primes.java
+++ Count Primes.java
@@ -5,7 +5,29 @@ Count the number of prime numbers less than a non-negative number, n.
 */
 
 public class Solution {
-    //bf. find prime. 1) to sqrt(n), 2)skip even number
+	public int countPrimes(int n) {
+        int cnt=0;
+        for (int i=1; i<n; i= i+2) {
+            if (isPrime(i)) {
+                cnt++;
+            }
+        }
+        if (n>2) cnt++;//care
+        return cnt;
+    }
+    
+    public boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n==2 || n==3) return true;
+        if (n % 2 == 0) return false;
+        for (int i=3; i*i <=n; i= i+2) {//care
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+	
+	//old
+	//bf. find prime. 1) to sqrt(n), 2)skip even number
     //mark off alg. space o(n), time do not know
     public int countPrimes(int n) {
         int cnt = 0;

8e3e702b589eb5930238e9a78a9c91369992b79f
mywbob <mywbob@gmail.com>
2015-09-21 20:10:52 -0400

    2

--- Valid Sudoku.java
+++ Valid Sudoku.java
@@ -7,42 +7,59 @@ The Sudoku board could be partially filled, where empty cells are filled with th
 
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character> set = new HashSet<Character>();
+        Set<Character> set = new HashSet<Character>();
         //check row
-        for (int i=0; i<9; i++) {
+        for (int i=0; i<board.length; i++) {
             set.clear();
-            for (int j=0; j<9; j++) {
-                if (board[i][j] == '.') continue;
-                if (set.contains(board[i][j])) return false;
-                else set.add(board[i][j]);
+            for (int j=0; j<board[0].length; j++) {
+                if (board[i][j] != '.') {
+                    if (!set.contains(board[i][j]) && board[i][j] >= '1' && board[i][j] <= '9') {
+                        set.add(board[i][j]);
+                    } else {
+                        return false;
+                    }
+                }
             }
         }
         
         //check col
-        for (int i=0; i<9; i++) {
+        for (int i=0; i<board.length; i++) {
             set.clear();
-            for (int j=0; j<9; j++) {
-                if (board[j][i] == '.') continue;
-                if (set.contains(board[j][i])) return false;
-                else set.add(board[j][i]);
+            for (int j=0; j<board[0].length; j++) {
+                if (board[j][i] != '.') {
+                    if (!set.contains(board[j][i]) && board[j][i] >= '1' && board[j][i] <= '9') {
+                        set.add(board[j][i]);
+                    } else {
+                        return false;
+                    }
+                }
             }
         }
         
-        //check sub
-        for (int offsetr = 0; offsetr < 3; offsetr++) {
-            for (int offsetc = 0; offsetc < 3; offsetc++) {
+        set.clear();
+        
+        
+        //check 9 small mats
+        for (int offsi=0; offsi<3; offsi++) {
+            for (int offsj=0; offsj<3; offsj++) {
                 set.clear();
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (board[i+3*offsetr][j+3*offsetc] == '.') continue;
-                        if (set.contains(board[i+3*offsetr][j+3*offsetc])) return false;
-                        else set.add(board[i+3*offsetr][j+3*offsetc]);
+                for (int i=0; i<3; i++) {
+                    for (int j=0; j<3; j++) {
+                        if (board[i+3*offsi][j+3*offsj] != '.') {
+                            if (!set.contains(board[i+3*offsi][j+3*offsj]) && board[i+3*offsi][j+3*offsj]>='1' && board[i+3*offsi][j+3*offsj]<='9') {
+                                set.add(board[i+3*offsi][j+3*offsj]);
+                            
+                            } else {
+                                return false;
+                            }
+                        }
                     }
                 }
             }
         }
         
         return true;
+
     }
 	
 }
